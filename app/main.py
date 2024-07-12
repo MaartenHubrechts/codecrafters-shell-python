b24252c0403c5ceee7937d54632410675b88809c
Maarten Hubrechts <maarten.hubrechts@hotmail.com>
2024-07-12 23:39:18 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,11 @@
 import sys
+from enum import Enum
+
+
+class Commands(Enum):
+    EXIT = "exit"
+    ECHO = "echo"
+    TYPE = "type"
 
 
 def main():
@@ -9,10 +16,15 @@ def main():
         # Wait for user input
         cmd = input()
         tokens = cmd.split()
-        if tokens[0] == "exit":
+        if tokens[0] == Commands.EXIT.value:
             break
-        elif tokens[0] == "echo":
+        elif tokens[0] == Commands.ECHO.value:
             sys.stdout.write(" ".join(tokens[1:]) + "\n")
+        elif tokens[0] == Commands.TYPE.value:
+            if tokens[1] in Commands:
+                sys.stdout.write(f"{tokens[1]} is a shell builtin\n")
+            else:
+                sys.stdout.write(f"{tokens[1]}: not found\n")
         else:
             sys.stdout.write(f"{cmd}: command not found\n")
             sys.stdout.flush()
